42809f383778a0987a492de4e1833ecd793c8c12
Marcelo Rigoto <marcelorigoto1@gmail.com>
2023-03-31 20:43:41 -0300

    aula 4

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name="BooBoo"
+          description="Gatinho Voando!"
+          music="Música Alta"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+          likes={444}
+          messages={555}
+          shares={666}
+          name="Marcelo"
+          description="Outro Gatinha, mas esse não voa"
+          music="Clap"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
